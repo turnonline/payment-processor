bfd56e66d4d715864341cae41db7d3d045ca8701
Aurel Medvegy <medvegy@turnonline.biz>
2021-09-18 13:24:10 +0200

    <ReferenceResolver as static final instead of transient field.

--- src/main/java/biz/turnonline/ecosystem/payment/service/revolut/webhook/TransactionCreatedTask.java
+++ src/main/java/biz/turnonline/ecosystem/payment/service/revolut/webhook/TransactionCreatedTask.java
@@ -68,15 +68,15 @@ public class TransactionCreatedTask
 {
     private static final Logger LOGGER = LoggerFactory.getLogger( TransactionCreatedTask.class );
 
-    private static final long serialVersionUID = 8438319760944126297L;
+    private static final long serialVersionUID = 382312044209828756L;
+
+    private static final ReferenceResolver referenceResolver = new ReferenceResolver();
 
     transient private PaymentConfig config;
 
     transient private RestFacade facade;
 
     transient private CategoryService categoryService;
-
-    transient private final ReferenceResolver referenceResolver = new ReferenceResolver();
 
     /**
      * Constructor.
